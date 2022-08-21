4989f3ad8d14085a91aec83a4c4c9e0929262bc5
tristan <mousy93@hotmail.com>
2022-08-21 11:30:31 +0800

    added mutations + queries to utils for db and tokens

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+      bookCount
+      password
+      email
+      username
+      _id
+    }
+  }
+`;
